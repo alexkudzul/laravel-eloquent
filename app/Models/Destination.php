ffc960e545129ddbc129acae7d779c753e77072b
alexkudzul <alexkudzul@gmail.com>
2023-08-12 12:12:35 -0600

    Eloquent Conventions

--- app/Models/Destination.php
+++ app/Models/Destination.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Destination extends Model
 {
     use HasFactory;
+
+    /**
+     * Algunas de las variables y constantes que se pueden modificar sino
+     * se sigue las convenciones de Laravel y Eloquent.
+     */
+    // const CREATED_AT = 'creation_date';
+    // const UPDATED_AT = 'updated_date';
+
+    // protected $connection = 'sqlite';
+    // protected $table = 'example_table';
+    // protected $primaryKey = 'identificator';
+    // protected $incrementing = false;
+    // protected $keyType = 'string';
+    // public $timestamps = false;
+    // protected $dateFormat = 'U';
+
+    /**
+     * attributes: agrega valores predeterminados para algunos de los atributos
+     */
+    protected $attributes = [
+        'name' => 'México',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Destination;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    $destination = new Destination();
+
+    return $destination;
 });
