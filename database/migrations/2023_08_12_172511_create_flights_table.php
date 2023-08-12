03e11b7e6026e7573a05c53159952a6469f19bde
alexkudzul <alexkudzul@gmail.com>
2023-08-12 12:46:30 -0600

    Added columns on flights migration

--- database/migrations/2023_08_12_172511_create_flights_table.php
+++ database/migrations/2023_08_12_172511_create_flights_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('flights', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('number');
+            $table->integer('legs');
+            $table->boolean('active')->default(true);
+            $table->boolean('departed')->default(false);
+            $table->timestamp('arrived_at');
+            $table->foreignId('destination_id')->constrained('destinations')->cascadeOnDelete();
             $table->timestamps();
         });
     }
