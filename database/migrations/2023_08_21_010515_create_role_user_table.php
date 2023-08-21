74efb4651f86b260083d1370e2fe2070ed8cb8f2
alexkudzul <alexkudzul@gmail.com>
2023-08-20 19:11:41 -0600

    Added columns for tables

--- database/migrations/2023_08_21_010515_create_role_user_table.php
+++ database/migrations/2023_08_21_010515_create_role_user_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('role_id')->constrained('roles')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->boolean('active')->default(1);
             $table->timestamps();
         });
     }
